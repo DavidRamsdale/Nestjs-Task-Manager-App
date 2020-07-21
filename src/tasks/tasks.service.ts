33e589bc1f5522f43ead0a07e64a8322f709c422
David Ramsdale <davidramsdale1991@gmail.com>
2020-07-21 20:18:29 +1000

    Add basic CRUD methods for tasks

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task-dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => {
+      return task.id === id;
+    });
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string) {
+    this.tasks = this.tasks.filter(task => {
+      return task.id !== id;
+    });
+  }
+}
